84e99c61cafd4e1162c9381101d383e20725eeb5
PrimusLV <k.drivnieks@inbox.lv>
2016-05-04 17:25:25 +0300

    Update Main.php

--- src/driesboy/Main.php
+++ src/driesboy/Main.php
@@ -4,16 +4,17 @@ namespace driesboy;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\Player;
-use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\entity\EntityLevelChangeEvent;
-use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\item\Item;
 // Utils
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
 // Events
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\player\PlayerItemHeldEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\entity\EntityLevelChangeEvent;
+use pocketmine\event\player\PlayerRespawnEvent;
 // Command
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
@@ -21,96 +22,123 @@ use pocketmine\command\ConsoleCommandSender;
 use pocketmine\command\CommandExecutor;
 
 class Main extends PluginBase implements Listener{
+	
+	protected $slots = [];
 
 	public function onEnable(){
 		@mkdir($this->getDataFolder());
                 $this->saveDefaultConfig();
-                $this->reloadConfig();
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		
+		// Load slots
+		$slots = $this->getConfig()->get('slots');
+		/*foreach($slots as $item => $pref){
+			
+		}*/
+		$this->slots = $slots;
+		
+		$this->getLogger()->info("Enabled");
 	}
+	
+	/**
+	 * @param PlayerItemHeldEvent $ev
+	 * @priority HIGHEST
+	 * @ignoreCancelled true
+	 */
 	public function PlayerItemHeld(PlayerItemHeldEvent $ev){
 		$player = $ev->getPlayer();
 		$item = $ev->getItem();
-        	if($item instanceof Item){
-          		if($item->getID() === $this->getConfig()->get("slot1")){
-          			$player->sendPopup($this->getConfig()->get("slot1-Tip"));
-          		}
-          		if($item->getID() === $this->getConfig()->get("slot2")){
-          			$player->sendPopup($this->getConfig()->get("slot2-Tip"));
-          		}
-          		if($item->getID() === $this->getConfig()->get("slot3")){
-          			$player->sendPopup($this->getConfig()->get("slot3-Tip"));
-          		}
-          		if($item->getID() === $this->getConfig()->get("slot4")){
-          			$player->sendPopup($this->getConfig()->get("slot4-Tip"));
-          		}
-          		if($item->getID() === $this->getConfig()->get("slot5")){
-          			$player->sendPopup($this->getConfig()->get("slot5-Tip"));
-          		}
-		 }
+        	if(($r = $this->isSlotItem($item))){
+        		if($r['tip'] != "") $player->sendTip($r['tip']);
+        		if($r['popup'] != "") $player->sendPopup($r['popup']);
+        	}
    	}
+   	
+   	/**
+   	 * @param PlayerInteractEvent $event
+   	 * @priority HIGHEST
+   	 * @ignoreCancelled true
+   	 */
 	public function onPlayerInteract(PlayerInteractEvent $event){
-		$p = $event->getPlayer();
-		$i = $event->getItem();
-		if($i->getID() === $this->getConfig()->get("slot1")){
-			$p->getServer()->dispatchCommand(new ConsoleCommandSender(), str_ireplace("{PLAYER}", $p->getName(), $this->getConfig()->get("slot1-command")));
-			if($this->getConfig()->get("ClearItemsAfterUse") === true){
-          				$p->getInventory()->clearAll();	
-          			}
-		}
-		if($i->getID() === $this->getConfig()->get("slot2")){
-			$p->getServer()->dispatchCommand(new ConsoleCommandSender(), str_ireplace("{PLAYER}", $p->getName(), $this->getConfig()->get("slot2-command")));
-			if($this->getConfig()->get("ClearItemsAfterUse") === true){
-          				$p->getInventory()->clearAll();	
-          			}
-		}
-		if($i->getID() === $this->getConfig()->get("slot3")){
-			$p->getServer()->dispatchCommand(new ConsoleCommandSender(), str_ireplace("{PLAYER}", $p->getName(), $this->getConfig()->get("slot3-command")));
-			if($this->getConfig()->get("ClearItemsAfterUse") === true){
-          				$p->getInventory()->clearAll();	
-          			}
-		}
-		if($i->getID() === $this->getConfig()->get("slot4")){
-			$p->getServer()->dispatchCommand(new ConsoleCommandSender(), str_ireplace("{PLAYER}", $p->getName(), $this->getConfig()->get("slot4-command")));
-			if($this->getConfig()->get("ClearItemsAfterUse") === true){
-          				$p->getInventory()->clearAll();	
-          			}
-		}
-		if($i->getID() === $this->getConfig()->get("slot5")){
-			$p->getServer()->dispatchCommand(new ConsoleCommandSender(), str_ireplace("{PLAYER}", $p->getName(), $this->getConfig()->get("slot5-command")));
-			if($this->getConfig()->get("ClearItemsAfterUse") === true){
-          				$p->getInventory()->clearAll();	
-          			}
+		$player = $event->getPlayer();
+		$item = $event->getItem();
+		if(($r = $this->isSlotItem($item))){
+			foreach($f['commands'] as $command){
+				// TODO: Variable replace with real values
+				$command = str_replace(["{PLAYER}"], [$player->getName()], $command);
+				
+				$this->getServer()->dispatchCommand(new ConsoleCommandSender(), $command);
+			}
+			if($this->getConfigValue("remove-item-on-use") == true){
+				# TODO: No time for this xD
+			}
 		}
 	}
+	
+	/**
+	 * @param PlayerJoinEvent $event
+	 * @priority HIGHEST
+	 */
 	public function onJoin(PlayerJoinEvent $event){
-		if($this->getConfig()->get("GiveItemsOnJoin") === true){
-			$p = $event->getPlayer();
-			$p->getInventory()->setItem(1, Item::get($this->getConfig()->get("slot1"),0,1));
-			$p->getInventory()->setHotbarSlotIndex(1,1);
-			$p->getInventory()->setItem(2, Item::get($this->getConfig()->get("slot2"),0,1));
-			$p->getInventory()->setHotbarSlotIndex(2,2);
-			$p->getInventory()->setItem(3, Item::get($this->getConfig()->get("slot3"),0,1));
-			$p->getInventory()->setHotbarSlotIndex(3,3);
-			$p->getInventory()->setItem(4, Item::get($this->getConfig()->get("slot4"),0,1));
-			$p->getInventory()->setHotbarSlotIndex(4,4);
-			$p->getInventory()->setItem(5, Item::get($this->getConfig()->get("slot5"),0,1));
-			$p->getInventory()->setHotbarSlotIndex(5,5);
+		if($this->getConfigValue("GiveItemsOnJoin") === true){
+			$this->giveItems($event->getPlayer());
 		}
 	}
+	
+	/**
+	 * @param PlayerRespawnEvent $event
+	 * @priority HIGHEST
+	 */
 	public function onRespawn(PlayerRespawnEvent $event){
-		$p = $event->getPlayer();
-		if($this->getConfig()->get("GiveItemsOnRespawn") === true){
-			$p->getInventory()->setItem(1, Item::get($this->getConfig()->get("slot1"),0,1));
-			$p->getInventory()->setHotbarSlotIndex(1,1);
-			$p->getInventory()->setItem(2, Item::get($this->getConfig()->get("slot2"),0,1));
-			$p->getInventory()->setHotbarSlotIndex(2,2);
-			$p->getInventory()->setItem(3, Item::get($this->getConfig()->get("slot3"),0,1));
-			$p->getInventory()->setHotbarSlotIndex(3,3);
-			$p->getInventory()->setItem(4, Item::get($this->getConfig()->get("slot4"),0,1));
-			$p->getInventory()->setHotbarSlotIndex(4,4);
-			$p->getInventory()->setItem(5, Item::get($this->getConfig()->get("slot5"),0,1));
-			$p->getInventory()->setHotbarSlotIndex(5,5);
+		if($this->getConfigValue("GiveItemsOnRespawn") === true){
+			$this->giveItems($event->getPlayer())
 		}
 	}
+	
+	/**
+	 * Gives player a slot items
+	 * 
+	 * @param Player $player
+	 */
+	public function giveItems(Player $player){
+		$i = 0; // Index
+		foreach($this->slots as $item => $prefs){
+			$itemData = explode(':', $item);
+			$itemData[1] = isset($itemData[1]) ? $itemData[1] : 0;
+		
+			$player->getInventory()->setItem($i, Item::get($itemData[0], $itemData[1], 1));
+			$i++;
+		}
+	}
+	
+	/**
+	 * Returns value from config file. This function is shortcut for Config::get($key)
+	 * 
+	 * @param string $key
+	 * @return mixed
+	 */
+	public function getConfigValue($key){
+		$r = $this->getConfig()->get($key);
+		
+		if($r == 'true' or $r == 'false'){
+			return (bool) $r; 
+		}
+		
+		return $r;
+	}
+	
+	/**
+	 * @param Item $item
+	 * @return bool|array
+	 */
+	public function isSlotItem(Item $item){
+		foreach($this->slots as $slot => $prefs){
+			$itemData = explode(':', $slot);
+			if($item->getId() == $itemData[0] and $item->getDamage() == $itemData[1]){
+				return $prefs;
+			}
+		}
+		return false;
+	}
+	
 }
